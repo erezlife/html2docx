a9084be2b0643182dd7fe868fd007484c24dd805
Jon Dufresne <jon.dufresne@gmail.com>
2019-03-27 08:22:36 -0700

    Use mypy to typecheck the package
    myypy is a static type checker for Python. As the project evolves, mypy
    can type check the code to help find common bugs. The project is small
    enough to introduce without much effort.
    
    For details on mypy, see the documentation:
    
    https://mypy.readthedocs.io/en/stable/index.html
    
    Reviewed-by: François Freitag

--- html2docx.py
+++ html2docx.py
@@ -1,8 +1,10 @@
 import io
 import re
 from html.parser import HTMLParser
+from typing import List, Tuple
 
-import docx
+from docx import Document
+from docx.text.paragraph import Paragraph
 
 WHITESPACE_RE = re.compile(r"\s+")
 
@@ -10,23 +12,23 @@ WHITESPACE_RE = re.compile(r"\s+")
 class HTML2Docx(HTMLParser):
     header_re = re.compile(r"^h[1-6]$")
 
-    def __init__(self, title):
+    def __init__(self, title: str):
         super().__init__()
-        self.doc = docx.Document()
+        self.doc = Document()
         self.doc.core_properties.title = title
 
-        self.p = None
-        self.runs = []
-        self.list_style = []
+        self.p: Paragraph = None
+        self.runs: List[Run] = []
+        self.list_style: List[str] = []
         self.bold = 0
         self.italic = 0
         self.underline = 0
 
-    def feed(self, data):
+    def feed(self, data: str) -> None:
         super().feed(data)
         self.finish()
 
-    def handle_starttag(self, tag, attrs):
+    def handle_starttag(self, tag: str, attrs: List[Tuple[str, str]]) -> None:
         if tag in ["p", "div"]:
             self.finish()
             self.p = self.doc.add_paragraph()
@@ -49,7 +51,7 @@ class HTML2Docx(HTMLParser):
         elif tag == "u":
             self.underline += 1
 
-    def handle_endtag(self, tag):
+    def handle_endtag(self, tag: str) -> None:
         if tag in ["p", "div", "li"] or self.header_re.match(tag):
             self.finish()
             self.p = None
@@ -62,11 +64,11 @@ class HTML2Docx(HTMLParser):
         elif tag == "u":
             self.underline -= 1
 
-    def handle_data(self, data):
+    def handle_data(self, data: str) -> None:
         if self.p or data.strip():
             self.out(data)
 
-    def out(self, data, strip_whitespace=True):
+    def out(self, data: str, strip_whitespace: bool = True) -> None:
         if not self.p:
             self.p = self.doc.add_paragraph()
 
@@ -79,7 +81,7 @@ class HTML2Docx(HTMLParser):
         )
         self.runs.append(run)
 
-    def finish(self):
+    def finish(self) -> None:
         """Collapse white space across runs."""
         prev_run = None
         for run in self.runs:
@@ -102,7 +104,7 @@ class HTML2Docx(HTMLParser):
         self.p = None
         self.runs = []
 
-    def add_list_style(self, style):
+    def add_list_style(self, style: str) -> None:
         self.finish()
         if self.list_style:
             # The template included by python-docx only has 3 list styles.
@@ -113,7 +115,12 @@ class HTML2Docx(HTMLParser):
 
 class Run:
     def __init__(
-        self, text, bold=False, italic=False, underline=False, strip_whitespace=True
+        self,
+        text: str,
+        bold: bool = False,
+        italic: bool = False,
+        underline: bool = False,
+        strip_whitespace: bool = True,
     ):
         self.text = text
         self.bold = bold
@@ -125,7 +132,7 @@ class Run:
         self.needs_space_suffix = bool(WHITESPACE_RE.match(text[-1]))
 
 
-def html2docx(content, title):
+def html2docx(content: str, title: str) -> io.BytesIO:
     """Convert valid HTML content to a docx document and return it as a
     io.BytesIO() object.
 
