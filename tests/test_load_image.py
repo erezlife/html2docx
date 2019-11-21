d81339aa7bd5c49616d5708c2527d9777acaef5e
François Freitag <mail@franek.fr>
2019-11-21 16:38:30 +0100

    Add support for base64 encoded images
    Reviewed-by: Jon Dufresne

--- tests/test_load_image.py
+++ tests/test_load_image.py
@@ -1,10 +1,11 @@
+import base64
 import urllib.error
 import urllib.request
 from unittest import mock
 
 from html2docx.image import load_image
 
-from .utils import PROJECT_DIR, TEST_DIR
+from .utils import PROJECT_DIR, TEST_DIR, generate_image
 
 broken_image = PROJECT_DIR / "html2docx" / "image-broken.png"
 broken_image_bytes = broken_image.read_bytes()
@@ -58,3 +59,47 @@ def test_bad_content_length(bad_content_length_server):
     image_data = load_image(bad_content_length_server.base_url)
     assert image_data.getbuffer() == broken_image_bytes
     assert bad_content_length_server.httpd.request_count == 1
+
+
+def test_inline_base64():
+    image = generate_image(width=1, height=1)
+    image_b64 = base64.b64encode(image.getbuffer()).decode()
+    src = f"data:image/png;base64,{image_b64}"
+    image_data = load_image(src)
+    assert image_data.getbuffer() == image.getbuffer()
+
+
+def test_inline_non_ascii():
+    src = "data:image/png;base64,🦝"
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
+
+
+def test_inline_non_base64():
+    src = "data:image/png;base64,https://example.org/"
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
+
+
+def test_inline_unknown_encoding():
+    src = "data:image/png;unknown,foobar"
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
+
+
+def test_inline_base64_marker_in_data():
+    src = "data:text/plain,this is not ;base64, encoded."
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
+
+
+def test_inline_missing_comma():
+    src = "data:image/png;base64https://example.org/"
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
+
+
+def test_unknown_scheme():
+    src = ""
+    image_data = load_image(src)
+    assert image_data.getbuffer() == broken_image_bytes
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,15 @@
 import pathlib
+from io import BytesIO
+
+from PIL import Image
 
 TEST_DIR = pathlib.Path(__file__).parent.resolve(strict=True)
 PROJECT_DIR = TEST_DIR.parent
+DPI = 72
+
+
+def generate_image(width: int, height: int, dpi=(DPI, DPI)) -> BytesIO:
+    data = BytesIO()
+    with Image.new("L", (width, height)) as image:
+        image.save(data, format="png", dpi=dpi)
+    return data
